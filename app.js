3aac62cd2fc68b2427e45e506031cea389786db3
Ramric Cardinal <ramriccardinal14@gmail.com>
2026-03-03 02:30:00 +0800

    Setup backend packages and folder structure

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//sample lang
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const setupSwagger = require("./swagger");
+
+const app = express();
+
+setupSwagger(app);
+app.use(express.json());
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
---
+++ swagger.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: { title: "Servana IT Web API", version: "1.0.0" },
+    servers: [{ url: `http://localhost:${process.env.PORT}` }],
+  },
+  apis: ["./swaggerdocs/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const setupSwagger = (app) => {
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+      swaggerOptions: {
+        filter: true,
+        docExpansion: "list",
+      },
+      customJsStr: `
+        window.addEventListener('load', function () {
+          const observer = new MutationObserver(function () {
+            const filterInput = document.querySelector('.operation-filter-input');
+            if (filterInput && !document.querySelector('#custom-filter-label')) {
+              filterInput.placeholder = 'Diri ka mag search ug tag name...';
+
+              const label = document.createElement('label');
+              label.id = 'custom-filter-label';
+              label.style.display = 'block';
+              label.style.marginBottom = '-15px';
+              label.style.color = '#666';
+              label.style.fontSize = '14px';
+              label.innerHTML = 'Tag names are <strong>CASE-SENSITIVE</strong>, type the exact format of every tag name you want to search, oki?:';
+
+              filterInput.parentNode.insertBefore(label, filterInput);
+              observer.disconnect();
+            }
+          });
+          observer.observe(document.body, { childList: true, subtree: true });
+        });
+      `,
+    }),
+  );
+};
+
+module.exports = setupSwagger;
