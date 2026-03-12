b838f45b34f51d1d3a0e85e53490cc45858abbda
Ramric Cardinal <ramriccardinal14@gmail.com>
2026-03-12 17:53:05 +0800

    Update .env

--- routes/systemadmin.routes.js
+++ routes/systemadmin.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const {get_SystemAdmin, create_SystemAdmin, update_SystemAdmin} = require("../controllers/systemadmin.controllers");
+const validateToken = require("../middlewares/validateToken");
 
-router.get("/", get_SystemAdmin);
-router.post("/", create_SystemAdmin);
-router.put("/:systemadminuuid", update_SystemAdmin);
+router.get("/", validateToken, get_SystemAdmin);
+router.post("/", validateToken, create_SystemAdmin);
+router.put("/:systemadminuuid", validateToken, update_SystemAdmin);
 
 module.exports = router;
