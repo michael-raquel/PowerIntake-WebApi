8d28463c35420c6cb393094e2976dc8bf2f7fdce
DanielGals <janjangallaron@gmail.com>
2026-03-21 00:44:26 +0800

    Added Validation JWT, Test

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { upload, upload_Image, delete_Image, download_Image } = require("../controllers/images.controllers");
-
-router.post("/upload", upload.single("image"), upload_Image);
-router.delete("/upload/:blobName", delete_Image);
-router.get("/download/:blobName", download_Image); 
+const validateToken = require("../middlewares/validateToken");
+router.post("/upload", validateToken, upload.single("image"), upload_Image);
+router.delete("/upload/:blobName", validateToken, delete_Image);
+router.get("/download/:blobName", validateToken, download_Image); 
 
 module.exports = router;
