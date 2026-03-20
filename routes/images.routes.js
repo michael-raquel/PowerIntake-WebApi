4c5555ed55182265659e74408830514a5226743b
DanielGals <janjangallaron@gmail.com>
2026-03-20 15:12:27 +0800

    Revert "Protect routes with validateToken middleware"
    This reverts commit 613549ba730feb01053ac806e5be2d22ae8ccbaf.

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { upload, upload_Image, delete_Image, download_Image } = require("../controllers/images.controllers");
-const validateToken = require("../middlewares/validateToken");
 
-router.post("/upload", validateToken, upload.single("image"), upload_Image);
-router.delete("/upload/:blobName", validateToken, delete_Image);
-router.get("/download/:blobName", validateToken, download_Image); 
+router.post("/upload", upload.single("image"), upload_Image);
+router.delete("/upload/:blobName", delete_Image);
+router.get("/download/:blobName", download_Image); 
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { get_AllUsers, get_UserById, get_UserManager, get_UserDirectReports,
-    get_UserFullProfile, get_AllUsersWithDetails, get_UserGroups, get_UserAppRoleAssignments, get_UserFromDb, sync_Users,
-    sync_AllTenantUsers } = require('../controllers/users.controllers');
-const validateToken = require('../middlewares/validateToken');
+        get_UserFullProfile, get_AllUsersWithDetails, get_UserGroups, get_UserAppRoleAssignments, get_UserFromDb, sync_Users,
+        sync_AllTenantUsers } = require('../controllers/users.controllers');
 
-router.get('/', validateToken, get_AllUsers);
-router.get('/profile', validateToken, get_UserById);
-router.get('/manager', validateToken, get_UserManager);
-router.get('/direct-reports', validateToken, get_UserDirectReports);
-router.get('/full-profile', validateToken, get_UserFullProfile);
-router.get('/all-users-with-details', validateToken, get_AllUsersWithDetails);
-router.get('/groups', validateToken, get_UserGroups);
-router.get('/app-role-assignments', validateToken, get_UserAppRoleAssignments);
-router.get('/db', validateToken, get_UserFromDb);
-router.post('/sync', validateToken, sync_Users);
-router.post('/sync-all-tenants', validateToken, sync_AllTenantUsers);
+router.get('/', get_AllUsers);
+router.get('/profile', get_UserById);
+router.get('/manager', get_UserManager);
+router.get('/direct-reports', get_UserDirectReports);
+router.get('/full-profile', get_UserFullProfile);
+router.get('/all-users-with-details', get_AllUsersWithDetails);
+router.get('/groups', get_UserGroups);
+router.get('/app-role-assignments', get_UserAppRoleAssignments);
+router.get('/db', get_UserFromDb);
+router.post('/sync', sync_Users);
+router.post('/sync-all-tenants', sync_AllTenantUsers);
 
 module.exports = router;
